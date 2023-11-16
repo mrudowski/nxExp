d207e8a35ebd8afe17adbde49b00a75144ac5430
Marcin Rudowski <marcin.rudowski@fingo.info>
2023-11-16 21:19:54 +0100

    JotaiPlayground

--- apps/sw-base/src/components/Details/Details.tsx
+++ apps/sw-base/src/components/Details/Details.tsx
@@ -1,10 +1,8 @@
-import {useAtom, useAtomValue, useSetAtom} from 'jotai';
 import {ReactNode} from 'react';
-
-import {counterAtom, loadableAsyncAtom, reverseCounterAtom} from '@/state/jotaiAtoms.ts';
 
 import Avatar from '../Avatar/Avatar.tsx';
 import styles from './Details.module.scss';
+import JotaiPlayground from './JotaiPlayground.tsx';
 
 type ListProps = {
   name: string;
@@ -12,32 +10,12 @@ type ListProps = {
 };
 
 const Details = ({name, children}: ListProps) => {
-  const [counter, setCounter] = useAtom(counterAtom);
-  const resetCounter = () => setCounter(0);
-  const reverseCounter = useSetAtom(reverseCounterAtom);
-  const loadableText = useAtomValue(loadableAsyncAtom);
-
   return (
     <article className={styles.details}>
       <h2>
         <Avatar name={name} /> {name}
-        <button
-          onClick={() => {
-            setCounter(counter => counter + 1);
-          }}
-        >
-          {counter} +
-        </button>
-        <button onClick={resetCounter}>reset</button>
-        <button
-          onClick={() => {
-            reverseCounter();
-          }}
-        >
-          reverse
-        </button>
-        {loadableText.state === 'hasData' ? loadableText.data : 'waiting…'}
       </h2>
+      <JotaiPlayground />
       {children}
     </article>
   );
---
+++ apps/sw-base/src/components/Details/JotaiPlayground.tsx
@@ -0,0 +1,33 @@
+import {useAtom, useAtomValue, useSetAtom} from 'jotai/index';
+
+import {counterAtom, loadableAsyncAtom, reverseCounterAtom} from '@/state/jotaiAtoms.ts';
+
+const JotaiPlayground = () => {
+  const [counter, setCounter] = useAtom(counterAtom);
+  const resetCounter = () => setCounter(0);
+  const reverseCounter = useSetAtom(reverseCounterAtom);
+  const loadableText = useAtomValue(loadableAsyncAtom);
+
+  return (
+    <div className="playground">
+      <button
+        onClick={() => {
+          setCounter(counter => counter + 1);
+        }}
+      >
+        {counter} +
+      </button>
+      <button onClick={resetCounter}>reset</button>
+      <button
+        onClick={() => {
+          reverseCounter();
+        }}
+      >
+        reverse
+      </button>
+      {loadableText.state === 'hasData' ? loadableText.data : 'waiting…'}
+    </div>
+  );
+};
+
+export default JotaiPlayground;
