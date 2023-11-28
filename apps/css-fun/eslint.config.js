b910442142db39c71ea6d7639a15a356c79ca9b3
Marcin Rudowski <marcin.rudowski@fingo.info>
2023-11-28 19:38:21 +0100

    css-fun

--- apps/css-fun/eslint.config.js
+++ apps/css-fun/eslint.config.js
@@ -1,23 +1,4 @@
-const {FlatCompat} = require('@eslint/eslintrc');
 const baseConfig = require('../../eslint.config.js');
-const js = require('@eslint/js');
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-});
-module.exports = [
-  ...baseConfig,
-  {
-    files: ['apps/css-fun/**/*.ts', 'apps/css-fun/**/*.tsx', 'apps/css-fun/**/*.js', 'apps/css-fun/**/*.jsx'],
-    rules: {},
-  },
-  {
-    files: ['apps/css-fun/**/*.ts', 'apps/css-fun/**/*.tsx'],
-    rules: {},
-  },
-  {
-    files: ['apps/css-fun/**/*.js', 'apps/css-fun/**/*.jsx'],
-    rules: {},
-  },
-  ...compat.extends('plugin:@nx/react'),
-];
+const reactConfig = require('../../eslint-react.config.js');
+
+module.exports = [...baseConfig, ...reactConfig];
--- apps/css-fun/src/app/page1/Breakouts.tsx
+++ apps/css-fun/src/app/page1/Breakouts.tsx
@@ -1,19 +1,28 @@
 import {clsx} from 'clsx';
 
 import styles from './Breakouts.module.scss';
+import pageStyles from './Page.module.scss';
 
 function Breakouts() {
   return (
     <main className={styles.resizer}>
       <article className={styles.wrapper}>
-        <h1>Some Heading</h1>
+        <h1 className={pageStyles.header}>
+          Some Heading <span>1</span>
+        </h1>
         <p>Some content and stuff</p>
         <img className={styles.fullBleed} alt="green leafs" src="/leafs.jpg" />
       </article>
 
       <article className={styles.content}>
         <h1>Some Heading</h1>
-        <p>Some content and stuff</p>
+        <p className={pageStyles.textWrap}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
         <p className={styles.popout}>Some popout stuff</p>
         <p>Some content and stuff</p>
         <p className={styles.feature}>Some feature stuff</p>
