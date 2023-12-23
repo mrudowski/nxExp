3891c9034de7e21da221adda9a5d112be2b82522
mrudowski <45997056+mrudowski@users.noreply.github.com>
2023-12-23 22:04:14 +0100

    The best tile (#2)
    * tile img
    * changing name to grass2

--- apps/zen-gardens/src/app/app.tsx
+++ apps/zen-gardens/src/app/app.tsx
@@ -7,8 +7,22 @@ import Tile from '@/components/Tile/Tile.tsx';
 
 export function App() {
   return (
-    <div>
-      <Tile />
+    <div style={{position: 'relative', display: 'flex', flexWrap: 'wrap'}}>
+      {Array(64)
+        .fill(0)
+        .map((el, index) => (
+          <Tile key={index} />
+        ))}
+
+      {/*<div style={{position: 'absolute', pointerEvents: 'none', top: (18 * 4) / 4, left: (16 * 4) / 2}}>*/}
+      {/*  <Tile />*/}
+      {/*</div>*/}
+      {/*<div style={{position: 'absolute', pointerEvents: 'none', top: (18 * 4) / 4, left: -(16 * 4) / 2}}>*/}
+      {/*  <Tile />*/}
+      {/*</div>*/}
+      {/*<div style={{position: 'absolute', pointerEvents: 'none', top: (18 * 4) / 2}}>*/}
+      {/*  <Tile />*/}
+      {/*</div>*/}
     </div>
   );
 }
