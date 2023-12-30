b3365b1ecadc47e7809699056126c1158bad88fc
Marcin Rudowski <marcin.rudowski@fingo.info>
2023-12-30 23:03:21 +0100

    ModeSwitch

--- apps/zen-gardens/src/app/actions/components/ModeSwitch/ModeSwitch.tsx
+++ apps/zen-gardens/src/app/actions/components/ModeSwitch/ModeSwitch.tsx
@@ -1,7 +1,42 @@
-import {ButtonGroup} from '@mantine/core';
+import {ActionIcon, useMantineColorScheme, useMantineTheme} from '@mantine/core';
+import {IconBrush, IconEraser, IconPointer} from '@tabler/icons-react';
+
+import Tooltip from '@/components/Tooltip/Tooltip';
+
+import styles from './styles.module.scss';
 
 const ModeSwitch = () => {
-  return <ButtonGroup />;
+  const theme = useMantineTheme();
+  const {colorScheme} = useMantineColorScheme();
+
+  const commonProps = {
+    variant: 'transparent',
+    radius: 'lg',
+    size: 'sm',
+    color: colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.dark[2],
+  };
+
+  return (
+    <Tooltip.Group>
+      <ActionIcon.Group className={styles.modeSwitch}>
+        <Tooltip label="Paint">
+          <ActionIcon {...commonProps} aria-label="Paint" color="">
+            <IconBrush size={22} />
+          </ActionIcon>
+        </Tooltip>
+        <Tooltip label="Erase">
+          <ActionIcon {...commonProps} aria-label="Erase">
+            <IconEraser size={22} />
+          </ActionIcon>
+        </Tooltip>
+        <Tooltip label="Select">
+          <ActionIcon {...commonProps} aria-label="Select">
+            <IconPointer size={22} />
+          </ActionIcon>
+        </Tooltip>
+      </ActionIcon.Group>
+    </Tooltip.Group>
+  );
 };
 
 export default ModeSwitch;
--- apps/zen-gardens/src/components/Tooltip/Tooltip.tsx
+++ apps/zen-gardens/src/components/Tooltip/Tooltip.tsx
@@ -4,4 +4,6 @@ const Tooltip = (props: TooltipProps) => {
   return <MantineTooltip withArrow={true} arrowSize={8} {...props} />;
 };
 
+Tooltip.Group = MantineTooltip.Group;
+
 export default Tooltip;
